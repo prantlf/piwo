0f5ccf62ed316f5207ad2bd379b1b63e7b2b1d35
Ferdinand Prantl <prantlf@gmail.com>
2024-12-21 10:13:39 +0100

    Remove docusaurus

--- assets/js/5e95c892.f5cecdcb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpiowo_website=self.webpackChunkpiowo_website||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(6540);var t=r(4164),u=r(1003),a=r(7559),c=r(2831),i=r(1217),o=r(4848);function n(e){return(0,o.jsx)(u.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,o.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
--- assets/js/readme.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-  const form = document.getElementById('login')
-  form.addEventListener('submit', event => {
-    event.preventDefault()
-    console.log('Login form:', new FormData(form))
-  })
-}, 1000)
